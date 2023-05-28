f6d64f26d75958b08fbd3df48ef29520f844f196
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-05-28 18:00:16 +0300

    feat: use refactoring and add styles for active nav link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,10 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import "./Header.scss";
 import headerLogo from "../../images/bannerImages/header-logo.svg";
 
 function Header() {
+    const location = useLocation();
     return (
         <header className="header">
             <div className="header__container">
@@ -13,22 +14,22 @@ function Header() {
                 <nav className="header__menu">
                     <ul className="header__menu-container">
                         <li className="header__menu-link">
-                            <Link to="/members" className="header__menu-link-item">
+                            <Link to="/members" className={`header__menu-link-item ${location.pathname === '/members' ? 'header__menu-link-item_active' : ''}`}>
                                 Members
                             </Link>
                         </li>
                         <li className="header__menu-link">
-                            <Link to="/albums" className="header__menu-link-item">
+                            <Link to="/albums" className={`header__menu-link-item ${location.pathname === '/albums' ? 'header__menu-link-item_active' : ''}`}>
                                 Albums
                             </Link>
                         </li>
                         <li className="header__menu-link">
-                            <Link to="/history" className="header__menu-link-item">
+                            <Link to="/history" className={`header__menu-link-item ${location.pathname === '/history' ? 'header__menu-link-item_active' : ''}`}>
                                 History
                             </Link>
                         </li>
                         <li className="header__menu-link">
-                            <Link to="/social-media" className="header__menu-link-item">
+                            <Link to="/social-media" className={`header__menu-link-item ${location.pathname === '/social-media' ? 'header__menu-link-item_active' : ''}`}>
                                 Social Media
                             </Link>
                         </li>
