54155c4d26dec22336610dc9dbfc42f9363a2b98
Ariana <74054656+VincitQuiPatitur@users.noreply.github.com>
2023-06-19 15:36:07 +0300

    feat: add connection to albums API

---
+++ src/utils/AlbumsApi.js
@@ -0,0 +1,15 @@
+import {URL} from './constants';
+
+export const getAlbums = () => {
+    return fetch(`${URL}/api/albums`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка при получении данных');
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error('Ошибка при получении данных:', error);
+            throw error;
+        });
+}
